a5290d90b775ca79640b8fc9ab7b35520e06531a
Casz146 <cas.de.groot@hotmail.com>
2018-07-06 09:17:22 -0300

    commit1

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', function(client) {
+    // console.log('Client connected...');
+
+    // client.emit('messages', {hello: 'world'});
+
+    client.on('messages', function(data) {
+        console.log(data);
+        client.broadcast.emit("messages", data);
+    });
+});
+
+app.get('/', function(req , res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+server.listen(8080);
